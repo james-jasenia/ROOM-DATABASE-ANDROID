af2b9108e31ec85cfe8465ee1f5636e25193d5c7
James <james.jasenia@gmail.com>
2021-01-05 17:34:14 +1100

    Created data access object inteface (DAO)

---
+++ app/src/main/java/com/example/roomprototype/data/ContactDao.java
@@ -0,0 +1,29 @@
+package com.example.roomprototype.data;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.roomprototype.model.Contact;
+
+import java.util.List;
+
+//Data Access Object Interface
+
+@Dao
+public interface ContactDao {
+
+    //CRUD Operations
+    @Insert (onConflict = OnConflictStrategy.IGNORE)
+    void insert(Contact contact);
+
+    @Query("DELETE FROM contact_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM contact_table ORDER BY name ASC")
+    LiveData<List<Contact>> getAllContacts();
+
+}
